a1c82b7d30720207b5fb6ac3a193b4d8af8ed74a
Rus1112 <teprust@mail.ru>
2022-03-02 23:46:12 +0300

    first commit

---
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -0,0 +1,112 @@
+package ru.iu3.fclient;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import java.util.Random;
+import ru.iu3.fclient.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'fclient' library on application startup.
+    static {
+        System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
+    }
+
+    private ActivityMainBinding binding;
+    ActivityResultLauncher activityResultLauncher;
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int res = initRng();
+        byte[] d_key = randomBytes(16);
+
+        Random random = new Random();
+
+        byte[] mas = new byte[20];
+        for (int i = 0; i < mas.length; ++i) {
+            mas[i] = (byte) ((byte) random.nextInt() % 255);
+        }
+
+        // Пример шифрованя данных (в отладчике)
+        byte[] encrypt_mas = encrypt(d_key, mas);
+
+        // Пример дешифрования данных (в отладчике)
+        byte[] decrypt_mas = decrypt(d_key, encrypt_mas);
+        // Example of a call to a native method
+        //TextView tv = findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //При StartActivity нет вывода сообщения
+        activityResultLauncher.launch(it);
+        //startActivity(it);
+        //Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        //byte[] key =
+         //       stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        //byte[] enc = encrypt(key,
+        //        stringToHex("000000000000000102"));
+        //byte[] dec = decrypt(key, enc);
+        //String s = new String(Hex.encodeHex(dec)).toUpperCase();
+        //Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+    /**
+     * A native method that is implemented by the 'fclient' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+}
