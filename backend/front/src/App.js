5ef6f57d96e5d8c8cab08e3646303b1af7a05be2
Rus1112 <teprust@mail.ru>
2022-04-17 00:22:23 +0300

    eighth commit

---
+++ backend/front/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+import {createBrowserHistory} from "history";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Router, Route } from "react-router";
+import '/home/rustam/AndroidStudioProjects/backend/front/src/App.css';
+import Home from "/home/rustam/AndroidStudioProjects/backend/front/src/components/Home";
+import Another_home from "/home/rustam/AndroidStudioProjects/backend/front/src/components/Another_home";
+import NavigationBar from "/home/rustam/AndroidStudioProjects/backend/front/src/components/NavigationBarClass";
+
+function App() {
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<NavigationBar />
+				<div className="container-fluid">
+					<Routes>
+						<Route path="home" element={<Home />}/>
+						{         }
+						<Route path="Another_Home" element={<Another_home />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+}
+export default App;
---
+++ backend/front/src/components/Another_home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Another_Home extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+        	
+        	
+            <div className="mt-5 me-auto">
+                <h2>https://github.com/teprust/rpo_22</h2>
+            </div>
+        )
+    }
+}
+
+export default Another_Home;
